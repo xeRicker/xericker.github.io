a695791dc97468b259c5c07dcfdf1ceef72362cf
xeRicker <xericker@gmail.com>
2025-04-08 14:48:06 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -116,7 +116,7 @@ function generujRaport() {
   // Generowanie obrazka
   html2canvas(document.getElementById("raport"), {
     scale: 2,
-    width: 1500,
+    width: 2000,
   }).then(canvas => {
     const link = document.createElement("a");
     const formatted = dataStr.replace(/\./g, "_");

b7b7a43de796d7a95baff54ff09e34722101c6d8
xeRicker <xericker@gmail.com>
2025-07-07 22:50:44 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+export const GITHUB_CONFIG = {
+  // Wklej tutaj swój Personal Access Token z GitHuba.
+  TOKEN: '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', // <--- WAŻNE: ZASTĄP TO SWOIM TOKENEM
+
+  // Wpisz nazwę użytkownika lub organizacji, do której należy repozytorium.
+  REPO_OWNER: 'xeRicker', // <--- np. 'jankowalski'
+
+  // Wpisz nazwę repozytorium, w którym będą zapisywane dane.
+  REPO_NAME: 'xericker.github.io' // <--- np. 'baza-produktow'
+};
+
+// Konfiguracja aplikacji
+export const APP_CONFIG = {
+    // Czas w milisekundach, po którym zapisane dane w localStorage wygasają
+    LOCAL_STORAGE_EXPIRATION: 15 * 60 * 1000, // 15 minut
+};
